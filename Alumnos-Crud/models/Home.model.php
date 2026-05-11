7f8c0cbe3249f9214152c1aeec9b3a8eca46baae
Daniel Virguez <daniel-virguez@outlook.com>
2026-05-11 14:47:41 -0400

    Modelo Home simple para dashboard.

---
+++ Alumnos-Crud/models/Home.model.php
@@ -0,0 +1,7 @@
+<?php
+class HomeModel {
+    // Aquí podrías tener funciones para obtener datos del dashboard.
+    public function getBienvenida() {
+        return "¡Bienvenido al sistema de gestión!";
+    }
+}
