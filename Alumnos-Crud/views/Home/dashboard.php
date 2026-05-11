a232c98dd015f1dbfc97625764207f788a806afb
Daniel Virguez <daniel-virguez@outlook.com>
2026-05-11 14:47:50 -0400

    Vista principal dashboard Home para sistema MVC.

---
+++ Alumnos-Crud/views/Home/dashboard.php
@@ -0,0 +1,6 @@
+<h2>Dashboard</h2>
+<p><?= isset($bienvenida) ? $bienvenida : '' ?></p>
+<ul>
+    <li><a href="index.php?action=alumno">Ir a Gestión de Alumnos</a></li>
+    <li><a href="index.php?action=docente">Ir a Gestión de Docentes</a></li>
+</ul>
