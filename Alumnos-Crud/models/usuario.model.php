7eb19920e0ccdb435caa1b5e4c38d465c4c417a1
Daniel Virguez <daniel-virguez@outlook.com>
2026-05-15 22:28:35 +0000

    Update usuario.model.php

--- Alumnos-Crud/models/usuario.model.php
+++ Alumnos-Crud/models/usuario.model.php
@@ -9,11 +9,11 @@ class UsuarioModel extends ModelBase
             $stm = $this->pdo->prepare("SELECT * FROM usuarios WHERE correo = ? LIMIT 1");
             $stm->execute([$correo]);
             $r = $stm->fetch(PDO::FETCH_OBJ);
-            if ($r && $r->password === $password) {
+           if ($r && password_verify($password, $r->password_hash)) {
                 $user = new Usuario();
                 $user->__SET('id', $r->id);
                 $user->__SET('correo', $r->correo);
-                $user->__SET('password', $r->password);
+                $user->__SET('password', $r->password_hash);
                 return $user;
             }
             return false;
